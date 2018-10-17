09ceb1a9a7dedea9efb5790c62500d92e0eaa2cd
yuki.kaneko <ykaneko204@gmail.com>
2018-10-17 02:24:47 +0000

    have ranking

--- resources/views/items/items.blade.php
+++ resources/views/items/items.blade.php
@@ -22,7 +22,13 @@
                         </div>
                         @if (isset($item->count))
                             <div class="panel-footer">
+                                @if ($type == 'want')
                                 <p class="text-center">{{ $key+1 }}位: {{ $item->count}} Wants</p>
+                                @endif
+                                
+                                @if ($type == 'have')
+                                <p class="text-center">{{ $key+1 }}位: {{ $item->count}} Haves</p>
+                                @endif
                             </div>
                         @endif
                     </div>
